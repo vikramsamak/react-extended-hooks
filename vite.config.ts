7eeca90e68e2403bbf18973732db136e5d711153
vikramsamak <vikramsamak02@gmail.com>
2024-12-11 20:59:28 +0530

    config for building lib.

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,13 @@ export default defineConfig({
   plugins: [
     react(),
     dts({
-      include: ["src/**/*.ts", "src/**/*.tsx"], // Include all TypeScript and TSX files
-      insertTypesEntry: true, // Insert a types entry in package.json
-      outDir: path.join(__dirname, "dist"), // Output directory for the .d.ts files
-      copyDtsFiles: true, // Copy other .d.ts files from dependencies
-      tsconfigPath: path.join(__dirname, "tsconfig.app.json"), // Path to your tsconfig file
-      exclude: ["**/*.test.ts", "**/*.test.tsx"], // Exclude test files from the declaration files
+      include: ["src/**/*.ts", "src/**/*.tsx"],
+      insertTypesEntry: true,
+      outDir: "dist",
+      copyDtsFiles: true,
+      rollupTypes: true,
+      tsconfigPath: "./tsconfig.app.json",
+      exclude: ["**/*.test.ts", "**/*.test.tsx"],
     }),
   ],
   build: {
