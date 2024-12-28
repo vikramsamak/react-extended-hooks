3d59ee0c1b23c17fd6a2ad68d6a16b596148042b
vikramsamak <vikramsamak02@gmail.com>
2024-12-28 09:39:39 +0530

    chore: shadcn initial setup

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,13 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
